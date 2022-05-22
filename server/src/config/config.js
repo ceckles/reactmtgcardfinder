87e31be35cd85b8963dfea77351bed3dfe0e70bb
Chad E <chad1020@gmail.com>
2022-05-21 21:59:20 -0500

    added annimation

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -2,7 +2,7 @@ export const PORT = 8080;
 export const environment = {
     development: {
         serverURL: `http://localhost:${PORT}/`,
-        dbString: 'mongodb+srv://monger:33Rrlpca!@!@@mtgdb.pb6hb.mongodb.net/mtgdb?retryWrites=true&w=majority'
+        dbString: `mongodb+srv://${process.env.DBUSER}:${process.env.DBPW}@mtgdb.pb6hb.mongodb.net/mtgdb?retryWrites=true&w=majority`
     },
     production: {
         serverURL: `http://localhost:${PORT}/`,
